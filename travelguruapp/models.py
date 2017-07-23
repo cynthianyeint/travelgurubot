06a1bfc8936196d4d9e3e6fcdf28f4b80494b08b
Cynthia Nyeint <cynthianyeint@Cynthias-MacBook-Pro.local>
2017-07-24 02:29:54 +0800

    add str method in models

--- travelguruapp/models.py
+++ travelguruapp/models.py
@@ -10,15 +10,27 @@ class General (models.Model):
 	promotion_one = models.CharField(max_length = 500, blank=False, null=True)
 	promotion_two = models.CharField(max_length = 500, blank=False, null=True)
 
+	def __str__(self):
+		return self.price
+
 class Place (models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	name = models.CharField(max_length=100, blank=False)
 	description = models.CharField(max_length=500, blank=False, null=True)
 
+	def __str__(self):
+		return self.name
+
 class Image (models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	url = models.CharField(max_length=200, blank=False)
 
+	def __str__(self):
+		return self.url
+
 class Rate (models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	rate = models.CharField(max_length=100, blank=True)
+
+	def __str__(self):
+		return self.rate
